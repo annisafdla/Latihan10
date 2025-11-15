b24940ac3658ce3c1cc36f7c71cefab6cb4d04fd
annisafdla <annisafdlahrp@gmail.com>
2025-11-15 09:03:29 -0800

    first commit

---
+++ routes/user.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+// GET semua user
+router.get('/', userController.getAllUsers);
+
+// GET user by ID
+router.get('/:id', userController.getUserById);
+
+// POST user baru
+router.post('/', userController.createUser);
+
+// PUT update user
+router.put('/:id', userController.updateUser);
+
+// DELETE user
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
